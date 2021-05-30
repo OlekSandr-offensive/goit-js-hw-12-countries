f690d9693484ff30b925a8e6285c6a8416e3ff99
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2021-05-30 23:05:43 +0200

    закинув лодеш

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './sass/main.scss';
+import countryCardTlp from './partials/country-card.hbs';
+import debounce from 'lodash.debounce';
+
+// const refs = {
+//   input: document.querySelector('[data-action="searchCountry"]'),
+// };
+
+// refs.input.addEventListener('input', debounce(onInputCountry, 500));
+
+// function onInputCountry() {}
+
+fetch('https://restcountries.eu/rest/v2/name/Colombia')
+  .then(response => {
+    return response.json();
+  })
+  .then(country => {
+    console.log(country);
+    const markup = countryCardTlp(country);
+    console.log(markup);
+  })
+  .catch(err => {
+    console.log(err);
+  });
