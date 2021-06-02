9e12eb00cfc28fd68bef713d46ef16c4e522bf73
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2021-06-02 12:46:57 +0200

    додав плагін

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import './sass/main.scss';
+import '@pnotify/core/dist/PNotify.css';
 import countryCardTlp from './partials/country-card.hbs';
 import debounce from 'lodash.debounce';
 import API from './partials/fetchCountries';
 import getRefs from './partials/get-refs';
+import {
+  alert,
+  defaultModules,
+} from '../node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile';
+
+defaultModules.set(PNotifyMobile, {});
+
+alert({
+  text: 'Notice me, senpai!',
+});
+// defaultModules.set(PNotifyMobile, {});
+
+// const myStack = new PNotify.Stack({ dir1: 'down', firstpos1: 25 });
+// PNotify.notice({
+//   text: 'Notice 1.',
+//   stack: myStack,
+// });
 
 const refs = getRefs();
 
@@ -22,7 +41,7 @@ function renderCountryCard(country) {
 }
 
 function onFetchError() {
-  alert({
-    text: 'Notice me, senpai!',
+  return notice({
+    text: "I'm a notice.",
   });
 }
