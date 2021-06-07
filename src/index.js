0f5b777c6b7674b5c2a22a0d1663184939ad41fc
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2021-06-07 22:35:23 +0200

    закинув плагін

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
-import './sass/main.scss';
+import { error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
+import './sass/main.scss';
 import countryCardTlp from './partials/country-card.hbs';
+import listCountryTlp from './partials/list-country-card.hbs';
 import debounce from 'lodash.debounce';
 import API from './partials/fetchCountries';
 import getRefs from './partials/get-refs';
-import {
-  alert,
-  defaultModules,
-} from '../node_modules/@pnotify/core/dist/PNotify.js';
-import * as PNotifyMobile from '@pnotify/mobile';
-
-defaultModules.set(PNotifyMobile, {});
-
-alert({
-  text: 'Too many matches found. Please enter a more specific query!',
-});
-defaultModules.set(PNotifyMobile, {});
-
-// const myStack = new PNotify.Stack({ dir1: 'down', firstpos1: 25 });
-// PNotify.notice({
-//   text: 'Notice 1.',
-//   stack: myStack,
-// });
 
 const refs = getRefs();
 
@@ -31,31 +15,28 @@ function onSearch(e) {
   e.preventDefault();
 
   const searchQuery = e.target.value;
-
-  API.fetchCountries(searchQuery)
-    .then(renderCountryCard)
-    .catch(onFetchError)
-    .finally(clearPageMarkup);
+  if (searchQuery.length === 0) {
+    refs.cardContainer.innerHTML = ' ';
+  }
+  API.fetchCountries(searchQuery).then(renderCountryCard).catch(onFetchError);
 }
 
 function renderCountryCard(country) {
-  const markup = countryCardTlp(country);
-  refs.cardContainer.innerHTML = markup;
-}
-
-function clearPageMarkup(e) {
-  if (e.target.value.length === 0) {
-    refs.cardContainer.innerHTML = ' ';
-    return;
+  if (country.length === 1) {
+    const markup = countryCardTlp(country);
+    refs.cardContainer.innerHTML = markup;
+  } else if (country.length < 10) {
+    const markup = listCountryTlp(country);
+    refs.cardContainer.innerHTML = markup;
+  } else {
+    onFetchError();
   }
-  // refs.cardContainer.innerHTML = ' ';
-  console.log('hello Vova');
 }
 
 function onFetchError() {
-  console.log('hello Sasha');
-  // return PNotify.error({
-  //   text: 'Too many matches found. Please enter a more specific query!',
-  //   modules: new Map([...PNotify.defaultModules, [PNotifyDesktop, {}]]),
-  // });
+  error({
+    title: 'Oh No!',
+    text: 'Too many matches found. Please enter a more specific query!',
+    delay: 4000,
+  });
 }
